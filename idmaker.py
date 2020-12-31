9d26dd6f213c5235f4b4403d5a202b0e91c0b244
mikohan <angara99@gmail.com>
2020-12-31 13:24:39 +0300

    Added libs

--- idmaker.py
+++ idmaker.py
@@ -6,4 +6,7 @@ url = "https://www.englishcoachchad.com/products/practice-paradise/categories/55
 r = requests.get(url)
 print(r.status_code)
 
-soup = bs(html, "html.parser")
+html = r.text
+print(html)
+
+# soup = bs(html, "html.parser")
