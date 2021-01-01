d0ed33120ed5d2c0e6e8288bfd9d899fe544eb07
mikohan <angara99@gmail.com>
2021-01-01 23:43:52 +0300

    set all

--- linkParser.py
+++ linkParser.py
@@ -1,8 +1,15 @@
 from bs4 import BeautifulSoup as bs
+import requests
+import re
 
+from request_test import URL
 
 with open("tmp.html", "r") as html:
     soup = bs(html, "html.parser")
-    divs_all = soup.find_all("div", class_="syllabus__item")
-    for div in divs_all:
-        print(div.a["href"])
+    # divs_all = soup.find_all("div", class_="syllabus__item")
+    f = open("single_page.html", "r")
+    content = f.read()
+    s = bs(content, "lxml")
+    d = s.find("div", class_="wistia_embed")
+    m = re.search('(wistia_async_)(\w+)"', str(d))
+    print(m.group(2))
