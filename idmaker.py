6adec33a000232a699f6835f1fec97ee86cce751
mikohan <angara99@gmail.com>
2021-01-01 21:01:54 +0300

    Set up form state

--- idmaker.py
+++ idmaker.py
@@ -10,13 +10,14 @@ from robobrowser import RoboBrowser
 credentials = ("angara99@gmail.com", "suki33338")
 # data = {"login": "angara99@gmail.com", "password": "suki33338"}
 url_login = "https://www.englishcoachchad.com/login"
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 
 
 url_course = (
     "https://www.englishcoachchad.com/products/practice-paradise/categories/553711"
 )
 
-browser = RoboBrowser(user_agent=MOBILE_USER_AGENT, parser="lxml")
+browser = RoboBrowser(user_agent=user_agent, parser="lxml")
 
 browser.open(url_login)
 
@@ -26,6 +27,7 @@ signup_form["member[email]"].value = "angara99@gmail.com"
 
 signup_form["member[password]"].value = "suki33338"
 browser.submit_form(signup_form)
+browser.follow_link(url_course)
 
 content = browser.parsed()
 
